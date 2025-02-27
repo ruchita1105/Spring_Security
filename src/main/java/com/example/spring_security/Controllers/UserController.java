24e6a5f2db5f4e2a1b94d7e84ceddf0ede3c0bca
Ruchita1105 <ruchissonawane30@gmail.com>
2025-02-27 16:25:15 +0530

    initial commit

---
+++ src/main/java/com/example/spring_security/Controllers/UserController.java
@@ -0,0 +1,40 @@
+package com.example.spring_security.Controllers;
+
+import com.example.spring_security.Models.User;
+import com.example.spring_security.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+
+
+    //all users
+    @GetMapping("/")
+    public List<User> getAllUsers()
+    {
+            return this.userService.getAllUsers();
+    }
+
+    //return single username
+    @GetMapping("/{username}")
+    public User getUser(@PathVariable("username") String username)
+    {
+        return  this.userService.getUser(username);
+
+    }
+
+    @PostMapping("/")
+    public  User add(@RequestBody User user)
+    {
+        return  this.userService.addUser(user);
+    }
+
+
+}
---
+++ src/main/java/com/example/spring_security/Services/UserService.java
@@ -0,0 +1,37 @@
+package com.example.spring_security.Services;
+
+import com.example.spring_security.Models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class UserService
+{
+    List<User> list = new ArrayList<>();
+
+    public UserService()
+    {
+        list.add(new User("abc" ,"abc123","abc@gmail,com"));
+        list.add(new User("xyz" ,"xyz123","abc@gmail,com"));
+        list.add(new User("pqr" ,"pqr123","pqr@gmail,com"));
+    }
+
+    //get all users
+    public List<User> getAllUsers() {
+        return this.list;
+    }
+
+    //get single user
+    public User getUser(String username)
+    {
+        return this.list.stream().filter(user -> user.getUsername().equals(username)).findAny().orElse(null);
+    }
+
+    //add new user
+    public User addUser(User user)
+    {
+        this.list.add(user);
+        return  user;
+    }
+}
